4305949e3e0a8a2dd22e3d1ca36a127c7b706204
Ganesh Bakkera <learnerforge@users.noreply.github.com>
2026-05-27 18:21:55 +0530

    auto-commit [scan] NexaSite: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: { allowedHosts: true },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
